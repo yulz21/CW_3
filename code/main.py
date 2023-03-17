e8745c6c1d8c2281ead5e9fe0a9add00858fc260
yuliazhukova <yuliazhukova@hotmail.com>
2023-03-17 18:31:52 +0100

    amend main function

--- code/main.py
+++ code/main.py
@@ -3,14 +3,11 @@ from code.utils import get_data, get_filtered_data, get_last_data, get_formatted
 
 def main():
 
-    OPERATIONS_URL = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678992616942&signature=iMLCdLiDNDIsEKIz-rIzWhW-v9X53p11o1g3rNfRbX4&downloadName=operations.json"
+    FILENAME = "operations.json"
     COUNT_LAST_VALUES = 5
     FILTERED_EMPTY_FROM = True
 
-    data, info = get_data(OPERATIONS_URL)
-    if not data:
-        exit(info)
-    print(info, end="\n\n")
+    data = get_data(FILENAME)
 
     data = get_filtered_data(data, FILTERED_EMPTY_FROM)
 
